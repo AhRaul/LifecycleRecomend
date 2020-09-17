cae95257abf5cca96175b83ca5c5e60e4bd76c38
AhRaul <ralchik@yandex.ru>
2020-09-17 20:00:17 +0300

    Get state methods.

--- app/src/main/java/com/example/lifecyclerecomend/LifeCyclePresenter.java
+++ app/src/main/java/com/example/lifecyclerecomend/LifeCyclePresenter.java
@@ -4,7 +4,10 @@ import android.util.Log;
 
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.OnLifecycleEvent;
+
+import static androidx.lifecycle.Lifecycle.Event.ON_ANY;
 
 // шаг 1: унаследовать implements LifecycleObserver
 public class LifeCyclePresenter implements LifecycleObserver {
@@ -13,12 +16,22 @@ public class LifeCyclePresenter implements LifecycleObserver {
     //шаг 2: аннотация @OnLifecycleEvent(Lifecycle.Event....)
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     public void onStartMethod() {
-        Log.d(TAG, "onStartMethod: ");
+        Log.d(TAG, "onStartMethod");
     }
 
     //шаг 2...
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     public void onStopMethod() {
-        Log.d(TAG, "onStopMethod: ");
+        Log.d(TAG, "onStopMethod");
+    }
+
+    //другие возможности______________________________________
+
+    //Вы можете использовать событие ON_ANY для получения всех событий в одном методе
+    //В этом случае все события будут вызывать этот метод.
+    //Используйте входящий параметр event, чтобы определить, какое именно событие произошло
+    @OnLifecycleEvent(ON_ANY)
+    void onAny(LifecycleOwner source, Lifecycle.Event event) {
+        Log.d(TAG, "onAny: " + event);
     }
 }
--- app/src/main/java/com/example/lifecyclerecomend/MainActivity.java
+++ app/src/main/java/com/example/lifecyclerecomend/MainActivity.java
@@ -1,21 +1,46 @@
 package com.example.lifecyclerecomend;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Lifecycle;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     LifeCyclePresenter lifeCyclePresenter;
+    Button bGetLifeCycleState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bGetLifeCycleState = findViewById(R.id.b_get_life_cycle_state);
 
         lifeCyclePresenter = new LifeCyclePresenter();
 
         //шаг 3: подписать LifeCyclePresenter на MainActivity
         getLifecycle().addObserver(lifeCyclePresenter);
+
+        //другие возможности______________________________________
+        //Состояния_______________________________________________
+        bGetLifeCycleState.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                //Если вы хотите узнать текущее состояние Activity, то у его объекта Lifecycle есть метод getCurrentState:
+                if(getLifecycle().getCurrentState() == Lifecycle.State.RESUMED) {
+                    Log.d(TAG, "onClick: isResumed");
+                }
+
+                //Также, вы можете проверить, что текущее состояние Activity не ниже определенного состояния.
+                //Метод isAtLeast здесь проверяет, что состояние Activity не ниже, чем STARTED. Т.е. либо STARTED, либо RESUMED.
+                if(getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED)) {
+                    Log.d(TAG, "onClick: состояние Activity не ниже, чем STARTED. Т.е. либо STARTED, либо RESUMED.");
+                }
+            }
+        });
     }
 }
